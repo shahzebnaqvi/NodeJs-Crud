db053a1a38523c15154b991974a19f8c109e7cd5
shahzebnaqvi <shahzeb.naqvi12@gmail.com>
2022-11-18 15:41:08 +0500

    check app update

--- repository/userRepository.js
+++ repository/userRepository.js
@@ -2,28 +2,28 @@ const pool = require('../dbconn')
 
 class UserRepo {
     async getAllTasks() {
-        return await pool.query(`select * from public.user`);
+        return await pool.query(`select * from public.newtable`);
     }
 
     async checkrtasksingle(email, password) {
-        return await pool.query(`select * from public.user  WHERE email = $1 and password = $2`, [email, password]
+        return await pool.query(`select * from public.newtable  WHERE email = $1 and password = $2`, [email, password]
         );
     }
 
 
     async createTaskRepo(email, password) {
-        return await pool.query(`INSERT INTO public.user
+        return await pool.query(`INSERT INTO public.newtable
         ( email, password )
         VALUES($1,$2)`,
             [email, password]);
     }
     async updateTaskRepo(id, email, password) {
-        return await pool.query(`UPDATE public.user
+        return await pool.query(`UPDATE public.newtable
         SET task = $2, done = $3 WHERE id = $1`,
             [id, email, password]);
     }
     async deleteTaskRepo(id) {
-        return await pool.query(`DELETE From public.user
+        return await pool.query(`DELETE From public.newtable
         WHERE id = $1`,
             [id]);
     }
